a84d9383c97be9744e1d32b91a7b172e8100e286
Celso <celso.machado@etec.sp.gov.br>
2024-05-10 01:41:28 -0300

    .

--- ipvext.py
+++ ipvext.py
@@ -1,7 +1,7 @@
 import socket
 
 def get_remote_machine_info():
-    remote_host = 'g1.globo.com'  # Use the hostname without the protocol
+    remote_host = 'eteccamargoaranha.com.br'  # Use the hostname without the protocol
     try:
         print("IP address: %s" % socket.gethostbyname(remote_host))
     except socket.error:
